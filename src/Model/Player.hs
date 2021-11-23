3ba326fac62afbf527616318d2ec6b7c21d16495
eestabaya <eestabay@ucsd.edu>
2021-11-22 20:35:16 -0800

    support deleting trail

--- src/Model/Player.hs
+++ src/Model/Player.hs
@@ -15,16 +15,35 @@ data Player = Player
 type Strategy = Pos     -- ^ current cursor
              -> Board   -- ^ current board
              -> XO      -- ^ naught or cross
-             -> IO Pos  -- ^ next move
+             -> IO (Pos, Maybe Pos)  -- ^ next move
 
 human :: Player 
-human = Player "human" (\p _ _ -> return p)
+human = Player "human" (\p _ _ -> return (p, Nothing))
 
 rando :: Player 
-rando = Player "machine" randomStrategy
+rando = Player "machine" travel
 
-randomStrategy :: a -> Board -> b -> IO Pos
-randomStrategy _ b _ = selectRandom (emptyPositions b) 
+--randomStrategy :: a -> Board -> b -> IO Pos
+--randomStrategy _ b _ = fetchZero (emptyPositions b)
+
+travel :: a -> Board -> b -> IO (Pos, Maybe Pos)
+travel _ b _ = deleteTrail b 0
+
+
+deleteTrail :: Board -> Int -> IO (Pos, Maybe Pos)
+deleteTrail b i
+  | i < dim = case b ! (Pos i 4) of
+
+    Just _ -> return (Pos (i + 1) 4, Just (Pos i 4))
+
+    _      -> deleteTrail b (i + 1)
+
+  | otherwise = return (Pos 1 4, Nothing)
+
+
+fetchZero :: [a] -> IO a
+fetchZero xs = do
+  return (xs !! 0)
 
 selectRandom :: [a] -> IO a
 selectRandom xs = do
